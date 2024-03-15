9c018dcd0cac42081cb0b165267dbc1ca55b9827
Heiko Kulinna <Heikokulinna@gmx.de>
2024-03-15 21:47:53 +0100

    [HK] initial commit 2

---
+++ dashapp/app/app.py
@@ -0,0 +1,79 @@
+
+
+import dash
+from dash import Dash, html, dcc
+from dash.long_callback import DiskcacheLongCallbackManager
+
+from app.utilities.sidebar_utils import (
+    icon_and_text
+)
+
+
+from flask import Flask
+
+# longcallbacks
+# diskcache
+import diskcache
+cache = diskcache.Cache("./cache")
+long_callback_manager = DiskcacheLongCallbackManager(cache)
+
+
+
+server = Flask(__name__)
+
+# session store
+a_session_store = dcc.Store(
+    id = "a_session_store", storage_type="session"
+)
+feature_limit_store = dcc.Store(
+    id = "feature_limit_store", storage_type="session"
+)
+
+
+# localhost/dashapp/pages_id
+url_base_pathname="/topology"
+
+app = Dash(
+    __name__,
+    server = server,
+    url_base_pathname="/topology/",
+    use_pages=True
+    )
+
+app.title = "Topology"
+
+
+
+
+
+sidebar = html.Div(
+    [
+        html.Div(
+            [
+                html.H2("Basic App", style={"color": "white"}),
+            ],
+            className="sidebar-header",
+            ),
+        html.Div(
+            dcc.Markdown("\n---\n")
+        ),
+        icon_and_text(id="side_home", text="Home", icon="home", href=url_base_pathname+dash.page_registry['pages.home']['path']),
+        icon_and_text(id="side_predict", text="Predict", icon="ai1", href=url_base_pathname+dash.page_registry['pages.predict.predict']['path']),
+        icon_and_text(id="side_optimizer", text="Optimize", icon="ai1", href=url_base_pathname+dash.page_registry['pages.optimizer.optimizer']['path']),
+    ],
+    className="sidebar"
+)
+
+app.layout = html.Div(
+    [
+        sidebar,
+        a_session_store,
+        html.Div(
+            [
+                dash.page_container
+            ],
+            className="content",
+        ),
+    ]
+)
+
--- experimental/make_a_graph_networkx/make_a_graph_and_app.py
+++ experimental/make_a_graph_networkx/make_a_graph_and_app.py
@@ -2,6 +2,36 @@
 
 
 
+# import the library
+
+import networkx as nx
+
+import plotly.graph_objects as go
+
+import pandas as pd
+
+import numpy as np
+
+import random
+
+import dash
+
+
+
+
+# ################
+
+# Create a graph
+
+# ################
+
+# Create an empty graph with no nodes and no edges
+
+G = nx.Graph()
+
+
+
+# add a single node
 
 
 
