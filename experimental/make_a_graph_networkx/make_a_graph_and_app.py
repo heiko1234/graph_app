96c3ed0ee141abc6b19b8217ca35d74608673c15
Heiko Kulinna <Heikokulinna@gmx.de>
2024-03-17 20:23:02 +0100

    [HK] styling, concept of containing Reactor to Areakey and Reactor

--- experimental/make_a_graph_networkx/make_a_graph_and_app.py
+++ experimental/make_a_graph_networkx/make_a_graph_and_app.py
@@ -86,22 +86,73 @@ G.add_node("Uvinul", classname="recipe", label="Uvinul")
 
 # make directed edges of equipment nodes to the process nodes
 
-G.add_edge("Tinuvin", "R101", classname="contains")
-G.add_edge("Tinuvin", "R102", classname="contains")
+
+G.add_edge("LU_MES", "Tinuvin", classname="contains")
+
+
+
+# copy node R101 and add it tot the graph, class = topology, value and label are the same, name of the node is "R101"
+
+G.add_node("Tinuvin_R101", classname="topology", label="R101")
+
+# connect node Tinuvin_R101 topology to R101 equipment
+
+G.add_edge("Tinuvin", "Tinuvin_R101", classname="contains")
+
+
+# copy node R102 and add it tot the graph, class = topology, value and label are the same, name of the node is "R102"
+G.add_node("Tinuvin_R102", classname="topology", label="R102")
+G.add_edge( "Tinuvin", "Tinuvin_R102", classname="contains")
+
+
+
+G.add_edge("Tinuvin_R101", "R101", classname="contains")
+G.add_edge("Tinuvin_R102", "R102", classname="contains")
+
+
+
+
+# G.add_edge("Tinuvin", "R101", classname="contains")
+# G.add_edge("Tinuvin", "R102", classname="contains")
 
 # make directed edge of equipment node to the process node, R101 -> R102
 
-G.add_edge("R101", "R102", classname="processconnection")
+G.add_edge("Tinuvin_R101", "Tinuvin_R102", classname="processconnection")
 
 
 # make directed edges of equipment nodes to the process nodes Uvinul
 
-G.add_edge("Uvinul", "R101", classname="contains")
-G.add_edge("Uvinul", "R102", classname="contains")
 
-# make directed edge of equipment node to the process node, R102 -> R101 
 
-G.add_edge("R102", "R101", classname="processconnection")
+
+G.add_node("Uvinul_R101", classname="topology", label="R101")
+G.add_node("Uvinul_R102", classname="topology", label="R102")
+
+G.add_edge("Uvinul", "Uvinul_R101", classname="contains")
+G.add_edge("Uvinul", "Uvinul_R102", classname="contains")
+
+
+G.add_edge("Uvinul_R102", "Uvinul_R101", classname="processconnection")
+
+
+G.add_edge("Uvinul_R101", "R101", classname="contains")
+G.add_edge("Uvinul_R102", "R102", classname="contains")
+
+
+
+# make an combination of nodes and edges analysis
+
+# print(G.nodes(data=True))
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -119,31 +170,86 @@ from dash import html
 
 elements = nx.readwrite.json_graph.cytoscape_data(G)['elements']
 
+
+
+base_figure_stylesheet = [
+    {
+        "selector": "node",
+        "style": {
+            "label": "data(label)",
+            "text-wrap": "wrap",
+            "shape": "round-rectangle",
+            "width": 90,
+            "height": 45,
+            "background-color": "white",
+            "border-color": "grey",
+            "border-width": 1,
+            "border-opacity": 1,
+            "text-halign": "center",
+            "text-valign": "center",
+            "font-size": 10,
+            "text-opacity": 1,
+            "color": "black",
+        }
+    },
+    {
+        "selector": "node[classname = 'areakey']",
+        "style": {
+            "background-color": "blue",
+        }
+    },
+    {
+        "selector": "node[classname = 'equipment']",
+        "style": {
+            "background-color": "green",
+            "color": "white",
+        }
+    },
+    {
+        "selector": "edge",
+        "style": {
+            "curve-style": "taxi",
+            "target-arrow-shape": "triangle",
+            "taxi-direction": "rightward",
+            "text-wrap": "wrap",
+            "text-background-color": "grey",
+            "text-background-opacity": 1,
+            "text-background-padding": 3,
+            "color": "black",
+            "font-size": 10,
+            "text-border-color": "grey",
+            "text-border-width": "1px",
+            "text-border-opacity": "0.8",
+        }
+    },
+    {
+        "selector": "edge[classname = 'contains']",
+        "style": {
+            "curve-style": "bezier",
+            "line-color": "blue",
+        }
+    }
+]
+
+
+
+
+
 # create app
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
+app.layout = html.Div(
+    # style={'width': '1200px', 'height': '800px', 'margin': 'auto', 'padding': '10px'},
+    style={'width': '100%', 'height': '100vh', "align-items": "center", "justify-content": "center", "display": "flex"},
+    children=[
     cyto.Cytoscape(
         id='cytoscape',
         elements=elements,
-        layout={'name': 'circle'},
+        # layout={'name': 'circle'},
+        style={'width': '1200px', 'height': '800px'},
         # style edges and nodes, make connections between nodes via taxis
-        stylesheet=[
-            {
-                'selector': 'node',
-                'style': {
-                    'background-color': '#11479e',
-                    'label': 'data(label)'
-                }
-            },
-            {
-                'selector': 'edge',
-                'style': {
-                    'line-color': '#9dbaea'
-                }
-            }
-        ],
+        stylesheet=base_figure_stylesheet
     )
 ])
 
